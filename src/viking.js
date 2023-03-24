0e0c811c8b7d0e3a2887c7900fb8de32667afba6
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-03-24 11:35:11 -0400

    Iteration #3 Complete, Iteration #4 and #5 half complete

--- src/viking.js
+++ src/viking.js
@@ -37,7 +37,70 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+
+  vikingArmy = [];
+  saxonArmy = [];
+
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+
+  } 
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+
+  }
+
+  vikingAttack() {
+  Saxon.receiveDamage(Viking.strength);
+
+  }
+
+  saxonAttack() {
+
+  }
+
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+      } else if (this.vikingArmy.length === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+      } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+      return `Vikings and Saxons are still in the thick of battle.`;
+      }
+      }
+
+}
+
+
+// if (this.saxonArmy.length = 0) {
+    //   return `Vikings have won the war of the century!`;
+    // } else if (this.vikingArmy.length = 0) {
+    //   return `Saxons have fought for their lives and survived another day...`;
+    // } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+    //   return `Vikings and Saxons are still in the thick of battle.`;
+    // }
+
+//     if (this.saxonArmy.length === 0) {
+//       return `Vikings have won the war of the century!`;
+//     } else if (this.vikingArmy.length === 0) {
+//       return `Saxons have fought for their lives and survived another day...`;
+//     } else if (this.saxonArmy.length >= 0 && this.vikingArmy.length >= 0) {
+//       return `Vikings and Saxons are still in the thick of battle.`;
+//     }
+// }
